e1b5f9e908adf6d40dedbe09e4e82b17b87c74c0
mamta <mamta.bansal.mail@gmail.com>
2020-11-05 10:16:53 +0100

    Add getUser

--- domino-rest-mplay-server/src/main/java/com/mamta/sl/server/resources/MyResource.java
+++ domino-rest-mplay-server/src/main/java/com/mamta/sl/server/resources/MyResource.java
@@ -4,15 +4,30 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.mamta.sl.shared.User;
 
 @Path("sl")
 public class MyResource {
 
 	@GET
-	@Path("user")
+	@Path("message")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getMessage() {
-		// return "Got it!";
-		return null;
+		return "Got it!";
+		//return null;
+	}
+	
+
+	@GET
+	@Path("user")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getUser() {
+		User user = new User();
+		user.setName("Mamta");
+		
+		user = null;
+		return Response.ok().entity(user).build();
 	}
 }
